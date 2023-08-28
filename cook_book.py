eeb015361e0b83d3f8da79439f205b8f3ed89164
Viktory <victoriyayakovleva85@gmail.com>
2023-08-28 17:14:36 +0300

    cook_book.py changed

--- cook_book.py
+++ cook_book.py
@@ -9,16 +9,25 @@ with open ('cook_book.txt', encoding='utf-8') as f:
             ingredients.append({'ingredient_name': name, 'quantity': quantity, 'measure': measure})
         cook_book.setdefault(dish_name, ingredients)
         empty = f.readline()
-    # print(cook_book)
-
+# print(cook_book)
+    
+ 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
-    for ingredients in cook_book:
-        for ingredient in ingredients:
-            ingredient_name = ingredient['ingredient_name']
-            quantity = int(ingredient['quantity']) * count
-            measure = ingredient['measure']
-        
-        shop_list.setdefault(ingredient, )
-    return shop_list
+    for dish in dishes:
+        if dish in cook_book:
+            ingredients = cook_book[dish]
+            for ingredient in ingredients:
+                ingredient_name = ingredient['ingredient_name']
+                quantity = int(ingredient['quantity']) * person_count
+                measure = ingredient['measure']
+                shop_list.setdefault(ingredient_name, {'measure':measure, 'quantity': quantity})
+        else:
+            print(f"Блюда '{dish}' нет в кулинарной книге.")
     print(shop_list)
+    return shop_list
+
+dishes = ['Омлет', 'Суп']
+person_count = 1
+get_shop_list_by_dishes(dishes, person_count)
+
