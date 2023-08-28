e6cc023b9ece5f64bad1ae673ed108f113d59b67
Viktory <victoriyayakovleva85@gmail.com>
2023-08-28 15:51:45 +0300

    cook_book

---
+++ cook_book.py
@@ -0,0 +1,24 @@
+cook_book = {}
+with open ('cook_book.txt', encoding='utf-8') as f:
+    for line in f:
+        dish_name = line.strip()
+        ingredients = []
+        count = f.readline()
+        for i in range (int(count)):
+            name, quantity, measure = f.readline().strip().split('|')
+            ingredients.append({'ingredient_name': name, 'quantity': quantity, 'measure': measure})
+        cook_book.setdefault(dish_name, ingredients)
+        empty = f.readline()
+    # print(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for ingredients in cook_book:
+        for ingredient in ingredients:
+            ingredient_name = ingredient['ingredient_name']
+            quantity = int(ingredient['quantity']) * count
+            measure = ingredient['measure']
+        
+        shop_list.setdefault(ingredient, )
+    return shop_list
+    print(shop_list)
